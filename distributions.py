fixed vlb computation order problem

distributions.py
@@ -69,7 +69,8 @@ class Gaussian(GibbsSampling, MeanField, Collapsed):
     ### Gibbs sampling
 
     def resample(self,data=[]):
-        self.mu, self.sigma = sample_niw(*self._posterior_hypparams(*self._get_statistics(data)))
+        self._mu_mf, self._sigma_mf = self.mu, self.sigma = \
+                sample_niw(*self._posterior_hypparams(*self._get_statistics(data)))
 
     def _get_statistics(self,data):
         assert isinstance(data,np.ndarray) or \
@@ -92,6 +93,8 @@ class Gaussian(GibbsSampling, MeanField, Collapsed):
 
     ### Mean Field
 
+    # NOTE my sumsq is Nk*Sk
+
     def meanfieldupdate(self,data,weights):
         assert getdatasize(data) > 0
         # update
@@ -99,6 +102,7 @@ class Gaussian(GibbsSampling, MeanField, Collapsed):
                 self._posterior_hypparams(*self._get_weighted_statistics(data,weights))
         self.mu, self.sigma = self._mu_mf, self._sigma_mf/(self._nu_mf - self.D - 1) # for plotting
 
+    def get_vlb(self):
         # return avg energy plus entropy, our contribution to the mean field
         # variational lower bound
         D = self.D
@@ -106,7 +110,7 @@ class Gaussian(GibbsSampling, MeanField, Collapsed):
         # see Eq. 10.77 in Bishop
         q_entropy = -1 * (0.5 * (loglmbdatilde + self.D * (np.log(self._kappa_mf/(2*np.pi))-1)) \
                 - invwishart_entropy(self._sigma_mf,self._nu_mf))
-        # see Equ. 10.74 in Bishop, we aren't summing over K
+        # see Eq. 10.74 in Bishop, we aren't summing over K
         p_avgengy = 0.5 * (D * np.log(self.kappa_0/(2*np.pi)) + loglmbdatilde \
                 - D*self.kappa_0/self._kappa_mf - self.kappa_0*self._nu_mf*\
                 np.dot(self._mu_mf - self.mu_0,np.linalg.solve(self._sigma_mf,self._mu_mf - self.mu_0))) \
@@ -538,7 +542,7 @@ class Multinomial(GibbsSampling, MeanField):
         assert (isinstance(alphav_0,np.ndarray) and alphav_0.ndim == 1) ^ \
                 (K is not None and alpha_0 is not None)
 
-        if isinstance(alphav_0,np.ndarray):
+        if alphav_0 is not None:
             self.alphav_0 = alphav_0
             self.K = alphav_0.shape[0]
         else:
@@ -583,6 +587,7 @@ class Multinomial(GibbsSampling, MeanField):
         self._alpha_mf = self._posterior_hypparams(*self._get_weighted_statistics(data,weights))
         self.weights = self._alpha_mf / self._alpha_mf.sum() # for plotting
 
+    def get_vlb(self):
         # return avg energy plus entropy, our contribution to the vlb
         # see Eq. 10.66 in Bishop
         logpitilde = self.expected_log_likelihood(np.arange(self.K))

internals/labels.py
@@ -35,8 +35,9 @@ class Labels(object):
         self.z = sample_discrete_from_log(scores,axis=1)
 
     def meanfieldupdate(self):
-        # update
         data, N, K = self.data, self.data.shape[0], len(self.components)
+
+        # update, see Eq. 10.67 in Bishop
         component_scores = np.zeros((N,K))
 
         for idx, c in enumerate(self.components):
@@ -48,12 +49,19 @@ class Labels(object):
         self.r = np.exp(logr - logr.max(1)[:,na])
         self.r /= self.r.sum(1)[:,na]
 
+    def get_vlb(self):
         # return avg energy plus entropy, our contribution to the mean field
         # variational lower bound
+        errs = np.seterr(invalid='ignore',divide='ignore')
         logr = np.log(self.r)
-        logr[np.isinf(logr)] = -9999999 # -np.inf * 0 = 0
-        q_entropy = -1*(self.r*logr).sum()
-        p_avgengy = (self.r * logpitilde).sum()
+        prod = self.r*logr
+        prod[np.isnan(prod)] = 0. # 0 * -inf = 0
+        np.seterr(**errs)
+
+        logpitilde = self.weights.expected_log_likelihood(np.arange(len(self.components)))
+
+        q_entropy = -1*(prod).sum()
+        p_avgengy = (self.r*logpitilde).sum()
 
         return p_avgengy + q_entropy
 

models.py
@@ -88,26 +88,35 @@ class Mixture(ModelGibbsSampling, ModelMeanField, Distribution):
                 'Components must implement MeanField'
         assert len(self.labels_list) > 0, 'Must have data to run MeanField'
 
-        vlb = 0. # variatinoal lower bound
-
+        ### update sweep!
         # ask labels to get weights over z, stored as l.r
         for l in self.labels_list:
-            vlb += l.meanfieldupdate()
+            l.meanfieldupdate()
 
         # pass the weights to pi
-        vlb += self.weights.meanfieldupdate(None,[l.r for l in self.labels_list]) # None is a placeholder
+        self.weights.meanfieldupdate(None,[l.r for l in self.labels_list]) # None is a placeholder
 
         # pass the weights to the components
         for idx, c in enumerate(self.components):
-            vlb += c.meanfieldupdate([l.data for l in self.labels_list],
+            c.meanfieldupdate([l.data for l in self.labels_list],
                     [l.r[:,idx] for l in self.labels_list])
 
+        ### get vlb!
+        vlb = 0
+
+        # get labels terms
+        vlb += sum(l.get_vlb() for l in self.labels_list)
+
+        # get pi term
+        vlb += self.weights.get_vlb()
+
+        # get components terms
+        vlb += sum(c.get_vlb() for c in self.components)
+
         # finally, need the evidence term in the vlb
-        component_scores = np.zeros(len(self.components))
-        for idx, c in enumerate(self.components):
-            component_scores[idx] = sum(c.expected_log_likelihood(l.data).sum(0)
-                    for l in self.labels_list)
-        vlb += 0.5 * (sum(l.r.sum(0) for l in self.labels_list) * component_scores).sum()
+        for l in self.labels_list:
+            vlb += 0.5 * (l.r.sum(0) * np.array([c.expected_log_likelihood(l.data).sum()
+                for c in self.components])).sum()
 
         return vlb
 

test.py
@@ -2,26 +2,54 @@ from __future__ import division
 import numpy as np
 np.seterr(invalid='raise')
 from matplotlib import pyplot as plt
+import copy
 
 import models, distributions
 
-blah = models.Mixture(alpha_0=1,
-        components=[distributions.Gaussian(mu_0=np.zeros(2),sigma_0=np.eye(2),kappa_0=0.1,nu_0=4)
+priormodel = models.Mixture(alpha_0=1,
+        components=[distributions.Gaussian(mu_0=np.zeros(2),sigma_0=np.eye(2),kappa_0=0.05,nu_0=5)
             for itr in range(30)])
 
-data = blah.rvs(300)
-
-blah.add_data(data)
-
-blah.plot()
-plt.title('initial zs')
-
-vals = [blah.meanfield_coordinate_descent_step() for itr in range(50)]
-
-blah.plot()
-plt.title('final awesomeness')
+data = priormodel.rvs(100)
 
 plt.figure()
-plt.plot(vals)
+plt.plot(data[:,0],data[:,1],'kx')
+plt.title('data')
+
+posteriormodel = models.Mixture(alpha_0=1,
+        components=[distributions.Gaussian(mu_0=np.zeros(2),sigma_0=np.eye(2),kappa_0=0.05,nu_0=5)
+            for itr in range(30)])
+
+posteriormodel.add_data(data)
+
+allvals = []
+allmodels = []
+for superitr in range(20):
+    vals = []
+    for itr in range(200):
+        vals.append(posteriormodel.meanfield_coordinate_descent_step())
+        if len(vals) > 1 and vals[-1] < vals[-2] + 100:
+            break
+    print '%d iterations' % (itr+1)
+    allvals.append(vals)
+    allmodels.append(copy.deepcopy(posteriormodel))
+    for itr in range(200):
+        posteriormodel.resample_model()
+
+
+plt.figure()
+for vals in allvals:
+    plt.plot(vals)
+
+bestmodel = np.argmax([lambda v: v[-1] for v in allvals])
+allmodels[bestmodel].plot()
+plt.title('bestmodel')
+
+worstmodel = np.argmin([lambda v: v[-1] in allvals])
+allmodels[worstmodel].plot()
+plt.title('worstmodel')
+
+allmodels[0].plot()
+plt.title('firstmodel')
 
 plt.show()