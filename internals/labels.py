fixed dumb vlb calculation error in models.py

internals/labels.py
@@ -60,8 +60,10 @@ class Labels(object):
 
         logpitilde = self.weights.expected_log_likelihood(np.arange(len(self.components)))
 
-        q_entropy = -1*(prod).sum()
+        q_entropy = min(-1*(prod.sum()),0.) # numerical
         p_avgengy = (self.r*logpitilde).sum()
+
+        assert q_entropy <= 0 and p_avgengy <= 0
 
         return p_avgengy + q_entropy
 

models.py
@@ -115,8 +115,8 @@ class Mixture(ModelGibbsSampling, ModelMeanField, Distribution):
 
         # finally, need the evidence term in the vlb
         for l in self.labels_list:
-            vlb += 0.5 * (l.r.sum(0) * np.array([c.expected_log_likelihood(l.data).sum()
-                for c in self.components])).sum()
+            vlb += 0.5 * np.sum([(r*c.expected_log_likelihood(l.data)).sum()
+                for c,r in zip(self.components,l.r.T)])
 
         # add in symmetry factor (if we're actually symmetric)
         if len(set(self.weights.weights)) == 1 and \

test.py
@@ -3,16 +3,20 @@ import numpy as np
 np.seterr(invalid='raise')
 from matplotlib import pyplot as plt
 import copy
+import operator
 
+from pymattutil.text import progprint_xrange
 from pybasicbayes import models, distributions
 
-alpha_0=0.5
-obs_hypparams=dict(mu_0=np.zeros(2),sigma_0=np.eye(2),kappa_0=0.05,nu_0=5)
+alpha_0=2.
+obs_hypparams=dict(mu_0=np.zeros(2),sigma_0=np.eye(2),kappa_0=0.1,nu_0=5)
 
 priormodel = models.Mixture(alpha_0=alpha_0,
         components=[distributions.Gaussian(**obs_hypparams) for itr in range(50)])
 
-data = priormodel.rvs(300)
+data = priormodel.rvs(200)
+
+del priormodel
 
 plt.figure()
 plt.plot(data[:,0],data[:,1],'kx')
@@ -27,14 +31,12 @@ allvals = []
 allmodels = []
 for superitr in range(10):
     vals = []
-    for itr in range(200):
+    for itr in progprint_xrange(100):
         vals.append(posteriormodel.meanfield_coordinate_descent_step())
-        if len(vals) > 1 and vals[-1] < vals[-2] + 100:
-            break
-    print '%d iterations' % (itr+1)
+    # print '%d iterations' % (itr+1)
     allvals.append(vals)
     allmodels.append(copy.deepcopy(posteriormodel))
-    for itr in range(200):
+    for itr in range(50):
         posteriormodel.resample_model()
 
 
@@ -42,15 +44,9 @@ plt.figure()
 for vals in allvals:
     plt.plot(vals)
 
-bestmodel = np.argmax([lambda v: v[-1] for v in allvals])
-allmodels[bestmodel].plot()
-plt.title('bestmodel')
+models_and_scores = sorted([(i,m,v) for i,(m,v)
+    in enumerate(zip(allmodels,allvals))],key=operator.itemgetter(1),reverse=True)
 
-worstmodel = np.argmin([lambda v: v[-1] in allvals])
-allmodels[worstmodel].plot()
-plt.title('worstmodel')
-
-allmodels[0].plot()
-plt.title('firstmodel')
+models_and_scores[0][1].plot()
 
 plt.show()