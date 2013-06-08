5ab6496695296e3f564268d07b613311aa8ff4f9
Matthew Johnson <mattjj@csail.mit.edu>
2013-06-08 17:33:17 -0400

    broke out mixin test parameters, added slow attr

--- testing/mixins.py
+++ testing/mixins.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import abc, os
 
+from nose.plugins.attrib import attr
 from ..util import testing
 
 class DistributionTester(object):
@@ -23,6 +24,11 @@ class BigDataGibbsTester(DistributionTester):
         pass
 
 
+    @property
+    def big_data_size(self):
+        return 10000
+
+
     def big_data_tests(self):
         for setting_idx, hypparam_dict in enumerate(self.hyperparameter_settings):
             yield self.check_big_data, setting_idx, hypparam_dict
@@ -31,7 +37,7 @@ class BigDataGibbsTester(DistributionTester):
         d1 = self.distribution_class(**hypparam_dict)
         d2 = self.distribution_class(**hypparam_dict)
 
-        data = d1.rvs(10000)
+        data = d1.rvs(self.big_data_size)
         d2.resample(data)
 
         assert self.params_close(d1,d2)
@@ -44,6 +50,28 @@ class GewekeGibbsTester(DistributionTester):
         pass
 
 
+    @property
+    def geweke_nsamples(self):
+        return 10000
+
+    @property
+    def geweke_data_size(self):
+        return 10
+
+    @property
+    def geweke_ntrials(self):
+        return 3
+
+    @property
+    def geweke_pval(self):
+        return 0.05
+
+    @property
+    def geweke_num_statistic_fails_to_tolerate(self):
+        return 1
+
+
+    @attr('slow')
     def geweke_tests(self):
         for setting_idx, hypparam_dict in enumerate(self.hyperparameter_settings):
             yield self.check_geweke, setting_idx, hypparam_dict
@@ -57,9 +85,8 @@ class GewekeGibbsTester(DistributionTester):
         plt.ioff()
         fig = plt.figure()
 
-        nsamples = 10000
-        data_size = 10
-        ntrials = 5
+        nsamples, data_size, ntrials = self.geweke_nsamples, \
+                self.geweke_data_size, self.geweke_ntrials
 
         num_statistic_fails = 0
         for trial in xrange(ntrials):
@@ -81,7 +108,7 @@ class GewekeGibbsTester(DistributionTester):
 
             testing.populations_eq_quantile_plot(forward_statistics,gibbs_statistics,fig=fig)
             try:
-                testing.assert_populations_eq_moments(forward_statistics,gibbs_statistics,pval=0.01)
+                testing.assert_populations_eq_moments(forward_statistics,gibbs_statistics,pval=self.geweke_pval)
             except AssertionError:
                 example_violating_means = np.mean(forward_statistics), np.mean(gibbs_statistics)
                 num_statistic_fails += 1
@@ -91,7 +118,7 @@ class GewekeGibbsTester(DistributionTester):
         mkdir(os.path.dirname(figpath))
         plt.savefig(figpath)
 
-        assert num_statistic_fails <= ntrials//2, \
+        assert num_statistic_fails <= self.geweke_num_statistic_fails_to_tolerate, \
                 'Geweke may have failed, check figures, e.g. %0.3f vs %0.3f' % example_violating_means
 
 
