robustness fix in gaussian likelihood computation

distributions.py
@@ -77,7 +77,7 @@ class _GaussianBase(object):
     def log_likelihood(self,x):
         mu, sigma, D = self.mu, self.sigma, self.mu.shape[0]
         sigma_chol = self.sigma_chol
-        bads = np.isnan(x).any(axis=1)
+        bads = np.isnan(np.atleast_2d(x)).any(axis=1)
         x = np.nan_to_num(x).reshape((-1,D)) - mu
         xs = scipy.linalg.solve_triangular(sigma_chol,x.T,lower=True)
         out = -1./2. * inner1d(xs.T,xs.T) - D/2*np.log(2*np.pi) \