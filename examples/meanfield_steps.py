c40974984e0b79e3496170be82aee363fbb2e03d
Matthew Johnson <mattjj@csail.mit.edu>
2013-10-16 16:10:58 -0400

    removed data loading from class demo

--- examples/meanfield_steps.py
+++ examples/meanfield_steps.py
@@ -5,7 +5,7 @@ plt.interactive(True)
 
 from pybasicbayes import models, distributions
 
-GENERATE_DATA = False
+GENERATE_DATA = True
 
 ###########################
 #  generate or load data  #
@@ -14,19 +14,16 @@ GENERATE_DATA = False
 alpha_0=5.0
 obs_hypparams=dict(mu_0=np.zeros(2),sigma_0=np.eye(2),kappa_0=0.05,nu_0=5)
 
-if GENERATE_DATA:
-    priormodel = models.Mixture(alpha_0=alpha_0,
-            components=[distributions.Gaussian(**obs_hypparams) for itr in range(30)])
-    data, _ = priormodel.generate(100)
-    del priormodel
-else:
-    data = np.loadtxt('data.txt')
+priormodel = models.Mixture(alpha_0=alpha_0,
+        components=[distributions.Gaussian(**obs_hypparams) for itr in range(30)])
+data, _ = priormodel.generate(100)
+del priormodel
 
 plt.figure()
 plt.plot(data[:,0],data[:,1],'kx')
 plt.title('data')
 
-raw_input()
+raw_input() # pause for effect
 
 ###############
 #  inference  #
@@ -42,7 +39,7 @@ plt.figure(2,figsize=(8,6))
 posteriormodel.plot()
 plt.figure(3,figsize=(8,6))
 while True:
-    if raw_input().lower() == 'break':
+    if raw_input().lower() == 'break': # pause at each iteration
         break
 
     vlb = posteriormodel.meanfield_coordinate_descent_step()
