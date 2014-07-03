fix sign error in DiagonalGaussian.log_likelihood

distributions.py
@@ -784,7 +784,7 @@ class DiagonalGaussian(_GaussianBase,GibbsSampling,MaxLikelihood,MeanField):
         return -1./2*(
                 (np.einsum('ij,ij,j->i',x,x,Js) - np.einsum('ij,j,j->i',x,2*mu,Js))
                 +
-                (mu**2*Js - np.log(2*np.pi*sigmas)).sum())
+                (mu**2*Js + np.log(2*np.pi*sigmas)).sum())
 
     def _posterior_hypparams(self,n,xbar,sumsq):
         mu_0, nus_0, alphas_0, betas_0 = self.mu_0, self.nus_0, self.alphas_0, self.betas_0