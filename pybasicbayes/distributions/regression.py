8a5b6f5860da400bc1b28ff15015531d1cf2f69b
Matthew Johnson <mattjj@csail.mit.edu>
2016-04-06 09:53:59 -0400

    add autoregression classes

--- pybasicbayes/distributions/regression.py
+++ pybasicbayes/distributions/regression.py
@@ -1,7 +1,8 @@
 from __future__ import division
 from builtins import zip
 from builtins import range
-__all__ = ['Regression', 'RegressionNonconj', 'ARDRegression']
+__all__ = ['Regression', 'RegressionNonconj', 'ARDRegression',
+           'AutoRegression', 'ARDAutoRegression']
 
 import numpy as np
 from numpy import newaxis as na
@@ -11,7 +12,7 @@ from pybasicbayes.abstractions import GibbsSampling, MaxLikelihood, \
 from pybasicbayes.util.stats import sample_gaussian, sample_mniw, \
     sample_invwishart, getdatasize, mniw_expectedstats, mniw_log_partitionfunction
 from pybasicbayes.util.general import blockarray, inv_psd, cumsum, \
-    all_none, any_none
+    all_none, any_none, AR_striding
 
 
 class Regression(GibbsSampling, MeanField, MaxLikelihood):
@@ -502,3 +503,51 @@ class ARDRegression(Regression):
         self.A = A
         self.sigma = sigma
         self.K_0 = K_0
+
+
+class _ARMixin(object):
+    @property
+    def nlags(self):
+        if not self.affine:
+            return self.D_in // self.D_out
+        else:
+            return (self.D_in - 1) // self.D_out
+
+    @property
+    def D(self):
+        return self.D_out
+
+    def rvs(self,lagged_data):
+        return super(_ARMixin,self).rvs(
+                x=np.atleast_2d(lagged_data.ravel()),return_xy=False)
+
+    def _get_statistics(self,data):
+        return super(_ARMixin,self)._get_statistics(
+                data=self._ensure_strided(data))
+
+    def _get_weighted_statistics(self,data,weights):
+        return super(_ARMixin,self)._get_weighted_statistics(
+                data=self._ensure_strided(data),weights=weights)
+
+    def log_likelihood(self,xy):
+        return super(_ARMixin,self).log_likelihood(self._ensure_strided(xy))
+
+    def _ensure_strided(self,data):
+        if isinstance(data,np.ndarray):
+            if data.shape[1] != self.D*(self.nlags+1):
+                data = AR_striding(data,self.nlags)
+            return data
+        else:
+            return [self._ensure_strided(d) for d in data]
+
+
+class AutoRegression(_ARMixin,Regression):
+    pass
+
+
+class ARDAutoRegression(_ARMixin,ARDRegression):
+    def __init__(self,M_0,**kwargs):
+        blocksizes = [M_0.shape[0]]*(M_0.shape[1] // M_0.shape[0]) \
+                + ([1] if M_0.shape[1] % M_0.shape[0] and M_0.shape[0] != 1 else [])
+        super(ARDAutoRegression,self).__init__(
+                M_0=M_0,blocksizes=blocksizes,**kwargs)
